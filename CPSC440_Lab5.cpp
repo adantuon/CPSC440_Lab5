a8922b512f83a9ba65c957a6803002b26e86a6db
adantuon <adantuon@mail.umw.edu>
2025-05-28 17:18:36 -0400

    Added shield to bitmap

--- CPSC440_Lab5.cpp
+++ CPSC440_Lab5.cpp
@@ -82,6 +82,7 @@ void create_pc_bitmap(ALLEGRO_BITMAP *pc_bmp[], ALLEGRO_DISPLAY *display) {
     Everything is one to the right
     Vertical lines: start one to the right (add 1) and add one down (add 1)
     Horizontal lines: start one down (add 1) and add one to the right (add 1)
+    Pixels: down one (add 1) and to the right one (add 1)
     */
 
     //Palette
@@ -89,6 +90,8 @@ void create_pc_bitmap(ALLEGRO_BITMAP *pc_bmp[], ALLEGRO_DISPLAY *display) {
     ALLEGRO_COLOR arcaneBlue = al_map_rgb(51, 164, 252);
     ALLEGRO_COLOR steel = al_map_rgb(113, 121, 126);
     ALLEGRO_COLOR wood = al_map_rgb(79, 32, 15);
+    ALLEGRO_COLOR blue = al_map_rgb(48, 92, 222);
+    ALLEGRO_COLOR gold = al_map_rgb(255, 215, 0);
 
     //Spear
     //SpearHead
@@ -112,4 +115,37 @@ void create_pc_bitmap(ALLEGRO_BITMAP *pc_bmp[], ALLEGRO_DISPLAY *display) {
     al_draw_line(48, 60, 53, 60, black, 1);
     al_draw_line(49, 60, 52, 60, arcaneBlue, 1);
     al_draw_line(49, 61, 52, 61, black, 1);
+
+    //Shield
+    al_draw_line(7, 18, 31, 18, black, 1);
+    al_draw_line(3, 19, 7, 19, black, 1);
+    al_draw_line(7, 19, 11, 19, blue, 1);
+    al_draw_line(14, 19, 18, 19, blue, 1);
+    al_draw_line(18, 19, 20, 19, gold, 1);
+    al_draw_line(20, 19, 24, 19, blue, 1);
+    al_draw_line(27, 19, 31, 19, blue, 1);
+    al_draw_line(31, 19, 35, 19, black, 1);
+    al_draw_line(1, 20, 3, 20, black, 1);
+    al_draw_line(3, 20, 6, 20, blue, 1);
+    al_draw_line(6, 20, 8, 20, gold, 1);
+    al_draw_line(8, 20, 12, 20, blue, 1);
+    al_draw_line(12, 20, 26, 20, gold, 1);
+    al_draw_line(26, 20, 30, 20, blue, 1);
+    al_draw_line(30, 20, 32, 20, gold, 1);
+    al_draw_line(32, 20, 35, 20, blue, 1);
+    al_draw_line(35, 20, 37, 20, black, 1);
+    al_draw_line(2, 21, 4, 21, black, 1);
+    al_draw_line(4, 21, 7, 21, blue, 1);
+    al_draw_line(7, 21, 9, 21, gold, 1);
+    al_draw_line(9, 21, 29, 21, blue, 1);
+    al_draw_line(29, 21, 31, 21, gold, 1);
+    al_draw_line(31, 21, 34, 21, blue, 1);
+    al_draw_line(34, 21, 36, 21, black, 1);
+    al_draw_line(3, 22, 35, 22, black, 1);
+    al_draw_pixel(12, 19, gold);
+    al_draw_pixel(13, 19, blue);
+    al_draw_pixel(14, 19, gold);
+    al_draw_pixel(25, 19, gold);
+    al_draw_pixel(26, 19, blue);
+    al_draw_pixel(27, 19, gold);
 }
